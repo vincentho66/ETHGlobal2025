f13df4bac6511a90075d7be98f44ee2bbd91fd22
summerr0007 <65068498+summerr0007@users.noreply.github.com>
2025-04-06 08:39:57 +0800

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-from backend.oneinch.getters import get_available_symbol_df, get_token_historical_prices
-print(os.getenv('PYTHONPATH'))
-print(os.getenv('ONEINCH_API_KEY'))
-print(get_token_historical_prices())
